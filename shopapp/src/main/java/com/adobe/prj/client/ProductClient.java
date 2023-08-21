1e86126dbaa4e294122589a0a13030f0b28ef31f
Banuprakash <banuprakashc@yahoo.co.in>
2023-08-21 16:47:04 +0530

    day 4

---
+++ shopapp/src/main/java/com/adobe/prj/client/ProductClient.java
@@ -0,0 +1,43 @@
+package com.adobe.prj.client;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.adobe.prj.entity.Product;
+import com.adobe.prj.service.OrderService;
+
+@Component
+public class ProductClient implements CommandLineRunner {
+	@Autowired
+	private OrderService service;
+	@Override
+	public void run(String... args) throws Exception {
+		// code gets executed as soon as Spring container is created
+		addProduct();
+		listProducts();
+		getProductById();
+	}
+	
+	private void addProduct() {
+		Product p = Product.builder().name("Macbook Pro").price(220000.00).quantity(100).build();
+		service.addProduct(p);
+		System.out.println("Product added successfully!!!");
+	}
+
+	private void listProducts() {
+		List<Product> products = service.getProducts();
+		for(Product p : products) {
+			System.out.println(p);
+		}
+	}
+	
+	private void getProductById() {
+		System.out.println("#####");
+			Product p = service.getProductById(2);
+			System.out.println(p);
+		System.out.println("#####");
+	}
+}
